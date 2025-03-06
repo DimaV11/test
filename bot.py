4a9efc68cc3c0f6995a776016d093c63dc0d7e9f
Dima <dmitriy_vostroknutov@mail.ru>
2025-03-06 17:54:28 +0300

    Комментарий к коммиту

--- bot.py
+++ bot.py
@@ -23,4 +23,8 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text) 
+    
 bot.infinity_polling(none_stop=True)
